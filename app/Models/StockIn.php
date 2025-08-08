0df2d8b0f762dca690760c88c05fe7562b8cc087
Danaryko <danar.rikoharits@gmail.com>
2025-08-08 21:11:59 +0700

    first commit

---
+++ app/Models/StockIn.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StockIn extends Model
+{
+    protected $fillable = [
+        'product_id',
+        'supplier_id',
+        'quantity',
+        'date_in',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+}
